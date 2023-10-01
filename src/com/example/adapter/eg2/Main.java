1c24c553284b7f2a530e4ec166481e5019cdab53
JordanCYX <2338257659@qq.com>
2023-10-01 15:12:32 +0800

    修改使用的包

--- src/com/example/adapter/eg2/Main.java
+++ src/com/example/adapter/eg2/Main.java
@@ -1,8 +1,5 @@
 package com.example.adapter.eg2;
 
-import com.example.adapter.eg1.Print;
-import com.example.adapter.eg1.PrintBanner;
-
 public class Main {
     public static void main(String[] args) {
         Print p = new PrintBanner("Hello");
